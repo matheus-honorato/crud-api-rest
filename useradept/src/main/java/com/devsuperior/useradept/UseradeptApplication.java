e7ca7f4be659fd21ec3cb97f62450aa96682e120
Matheus Honorato <matheuswebmw@gmail.com>
2023-03-31 00:01:30 -0300

    Configurações padrão

---
+++ useradept/src/main/java/com/devsuperior/useradept/UseradeptApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.useradept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseradeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseradeptApplication.class, args);
+	}
+
+}
---
+++ useradept/src/test/java/com/devsuperior/useradept/UseradeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.useradept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseradeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
